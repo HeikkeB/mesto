a2c5abaf35dcf0729443e28cb7634a9290a2ab7b
Vladimir Boltin <boltinvlad@yandex.ru>
2022-07-16 00:27:46 +0300

    fix: form add

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-//popup
-const popup = document.querySelector('.popup');
 //template
 const template = document.querySelector('.template__card');
 //list cards
@@ -8,7 +6,7 @@ const cards = document.querySelector('.elements__list');
 //popup edit profile
 const editPopup = document.querySelector('.popup_edit');
 const popupEditForm = document.querySelector('.popup__form_edit')
-const popupClosedBtn = popup.querySelector('.popup__closed');
+const editPopupClosedBtn = document.querySelector('.popup__closed_edit');
 const profileBtnEdit = document.querySelector('.profile__btn-edit');
 const popupUsername = document.querySelector('.popup__input_name');
 const popupProfession = document.querySelector('.popup__input_profession');
@@ -17,23 +15,27 @@ const profileUsername = document.querySelector('.profile__username');
 
 //popup add card
 const addPopup = document.querySelector('.popup_add');
+const addPopupOpenBtn = document.querySelector('.profile__btn-add');
+const addPopupCloseBtn = document.querySelector('.popup__closed_add');
+const addPopupSaveBtn = document.querySelector('.popup__form_add');
+const cardNamePlace = document.querySelector('.popup__input_place');
+const cardLinkPlace = document.querySelector('.popup__input_link');
 
 
 //common open popups
-const openPopup = () => {
-    loadUserData();
-    popup.classList.add('popup_opened');
+const openPopup = item => {
+    item.classList.add('popup_opened');
 };
 
-const closePopup = () => {
-    popup.classList.remove('popup_opened');
+const closePopup = item => {
+    item.classList.remove('popup_opened');
 };
 
 //EDIT POPUP
 //open
 const openEditPopup = () => {
+    loadUserData();
     openPopup(editPopup);
-
 };
 
 //close
@@ -59,11 +61,26 @@ const submitPopupEdit = (evt) => {
 // Listener popup edit
 profileBtnEdit.addEventListener('click', openEditPopup);
 
-popupClosedBtn.addEventListener('click', closeEditPopup);
+editPopupClosedBtn.addEventListener('click', closeEditPopup);
 
 popupEditForm.addEventListener('submit', submitPopupEdit);
 
 
+//ADD POPUP 
+//open 
+const openAddPopup = () => {
+    openPopup(addPopup);
+};
+
+//close
+const closeAddPopup = () => {
+    closePopup(addPopup);
+}
+
+//Listener popup add
+addPopupOpenBtn.addEventListener('click', openAddPopup);
+
+addPopupCloseBtn.addEventListener('click', closeAddPopup);
 
 const initialCards = [
     {
@@ -91,4 +108,47 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+
+
+
+const initializeList = list => {
+    list.forEach(function (item) {
+        const post = createNewPost(item.name, item.link);
+        postsList.prepend(post);
+    });
+};
+
+initializeList(initialCards);
+
+/*//create card
+const createCard = ({ name, link }) => {
+    const card = template.content.cloneNode(true);
+    const cardImg = card.querySelector('.element__img');
+    const cardTitle = card.querySelector('.element__title');
+    cardTitle.innerText = name;
+    cardImg.src = link;
+    cardImg.alt = name;
+    createCardListeners(card);
+    return card;
+};
+
+const renderInitialCards = () => {
+    initialCards.forEach(({ name, link }) => {
+        const card = createCard({ name, link });
+        cards.appendChild(card);
+    });
+};
+
+renderInitialCards();
+
+//add card
+const addCard = (name, link) => {
+    const card = createCard({ name, link });
+    cards.prepend(card);
+};
+
+const submitAddBtn = (item) => {
+    addCard(cardNamePlace.value, cardLinkPlace.value);
+    closeAddPopup(item);
+}*/
 
