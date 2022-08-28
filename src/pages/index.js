257d0a7174df20b1f467fe609b8eda373803886d
Vladimir Boltin <boltinvlad@yandex.ru>
2022-08-28 17:37:37 +0300

    feat: make logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import { FormValidator } from './FormValidator.js';
-import Card from './Card.js';
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
+import { FormValidator } from '../components/FormValidator.js';
+import Card from '../components/Card.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 //list cards
 const cardsContainer = document.querySelector('.elements__list');
